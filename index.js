e109da3ffe279186d9f6ebc3be5be1603c7ccf27
Betsy Toyo <btoyo6009@e-camp.cl>
2022-05-17 22:13:09 -0400

    modificacion de los views

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 const hbs = require("express-handlebars");
-
-app.listen(3000, () => console.log('Your app listening on port 3000'))
+const jwt = require('jsonwebtoken');
+app.listen(3000, () => console.log('Your app listening on port 3000'));
 
 app.use(express.json());
 
 app.set("view engine", "handlebars");
+
+app.use("/css", express.static(`${__dirname}`));
 
 app.engine(
   "handlebars",
@@ -14,14 +16,18 @@ app.engine(
     layoutsDir: `${__dirname}/views`,
     partialsDir:`${__dirname}/views/partials`,
   })
-)
-
-
+);
 
 app.get("/", async (request, response) => {
-    response.render("inicio", {
-      layout: "inicio",    
-      
+    response.render("index", {
+      layout: "index",          
     });
   });
-  
+
+app.get('/login', (req, res) => {
+  res.render("login",{ layout: 'login'});
+})
+
+app.get('/registro', (req, res) => {
+  res.render("registro",{ layout: 'registro'});
+})
