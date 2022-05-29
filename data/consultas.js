6cc5755e2fb5150d59a8a7f755909cc239823143
Betsy Toyo <btoyo6009@e-camp.cl>
2022-05-29 17:00:49 -0400

    modificacion clave

--- data/consultas.js
+++ data/consultas.js
@@ -1,74 +1,88 @@
-const {Pool} = require('pg')
+const { Pool } = require("pg");
 
 const pool = new Pool({
-    user:"postgres",
-    password:"*****",
-    database:"skatepark",
-    host:"localhost",
-    port: 5432
-    
-})
+  user: "postgres",
+  password: "****y",
+  database: "skatepark",
+  host: "localhost",
+  port: 5432,
+});
 
-const registro= async (datos)=>{
-    const {email, nombre, password, experiencia, especialidad, nombreImagen}= datos
-    const parametros={
-        text:'INSERT INTO skaters(email, nombre, password, anos_experiencia, especialidad, foto, estado) VALUES($1, $2, $3, $4, $5, $6, $7) RETURNING*',     
-        values: [email, nombre, password, experiencia, especialidad, nombreImagen, false]       
-    }
+const registro = async (datos) => {
+  const { email, nombre, password, experiencia, especialidad, nombreImagen } =
+    datos;
+  const parametros = {
+    text: "INSERT INTO skaters(email, nombre, password, anos_experiencia, especialidad, foto, estado) VALUES($1, $2, $3, $4, $5, $6, $7) RETURNING*",
+    values: [
+      email,
+      nombre,
+      password,
+      experiencia,
+      especialidad,
+      nombreImagen,
+      false,
+    ],
+  };
 
-    try {
-        const { rows }= await pool.query(parametros)
-        return rows
-    } catch (error) {
-        return { status: 'ERROR', mensaje: error.message }
-    }
-}
+  try {
+    const { rows } = await pool.query(parametros);
+    return rows;
+  } catch (error) {
+    return { status: "ERROR", mensaje: error.message };
+  }
+};
 
-const registros= async ()=>{    
-    const parametros={
-        text:'SELECT * FROM skaters',     
-        values: []       
-    }
+const registros = async () => {
+  const parametros = {
+    text: "SELECT * FROM skaters",
+    values: [],
+  };
 
-    try {
-        const { rows }= await pool.query(parametros)
-        return rows
-    } catch (error) {
-        return { status: 'ERROR', mensaje: error.message }
-    }
-}
+  try {
+    const { rows } = await pool.query(parametros);
+    return rows;
+  } catch (error) {
+    return { status: "ERROR", mensaje: error.message };
+  }
+};
 
-const editarRegistro= async (datos)=>{
-    const {nombre, password, experiencia, especialidad, id}= datos
-    const parametros={
-        text:'UPDATE skaters SET nombre= $1, password= $2, anos_experiencia=$3, especialidad=$4 WHERE id= $5 RETURNING*',     
-        values: [nombre, password, experiencia, especialidad, id]       
-    }
-   
-    try {
-        const { rows }= await pool.query(parametros)
-        return rows
-    } catch (error) {
-        return { status: 'ERROR', mensaje: error.message }
-    }
-}
+const editarRegistro = async (datos) => {
+  const { nombre, password, experiencia, especialidad, id } = datos;
+  const parametros = {
+    text: "UPDATE skaters SET nombre= $1, password= $2, anos_experiencia=$3, especialidad=$4 WHERE id= $5 RETURNING*",
+    values: [nombre, password, experiencia, especialidad, id],
+  };
 
-const eliminarRegistro= async (id)=>{    
-    const sql= `DELETE FROM skaters WHERE id=${id}`
-    try {
-        const { rows }= await pool.query(sql)
-        return rows
-    } catch (error) {
-        return { status: 'ERROR', mensaje: error.message }
-    }
-}
+  try {
+    const { rows } = await pool.query(parametros);
+    return rows;
+  } catch (error) {
+    return { status: "ERROR", mensaje: error.message };
+  }
+};
+
+const eliminarRegistro = async (id) => {
+  const sql = `DELETE FROM skaters WHERE id=${id}`;
+  try {
+    const { rows } = await pool.query(sql);
+    return rows;
+  } catch (error) {
+    return { status: "ERROR", mensaje: error.message };
+  }
+};
 
 const skaterStatus = async (id, estado) => {
-    const result = await pool.query(
-      `UPDATE skaters SET estado = ${estado} WHERE id = ${id} RETURNING *`
-    );
-    const skater = result.rows[0];
-    return skater;
-  }
+  const result = await pool.query(
+    `UPDATE skaters SET estado = ${estado} WHERE id = ${id} RETURNING *`
+  );
+  const skater = result.rows[0];
+  return skater;
+};
 
-module.exports= {registro, registros, editarRegistro, eliminarRegistro, skaterStatus}
+module.exports = {
+  registro,
+  registros,
+  editarRegistro,
+  eliminarRegistro,
+  skaterStatus,
+};
